3b778612eb6764c871c81d1a883d31456e1c0b11
Ilya Volkov <illvolkov@bk.ru>
2022-01-18 18:15:17 +0500

    Initial Commit

---
+++ VKProfileScreenLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKProfileScreenLayout
+//
+//  Created by Ilya Volkov on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
